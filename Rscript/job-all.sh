5615bbda54766e7c8000fc2bbf2b2e9f14cf686d
ecor <emanuele.cordano@gmail.com>
2016-11-07 14:15:14 +0100

    updates

---
+++ Rscript/job-all.sh
@@ -0,0 +1,27 @@
+#$ -N geotopoptim2
+#$ -V
+#$ -pe mpich 32
+#$ -l h_rt=01:00:00
+#$ -M emanuele.cordano@gmail.com
+#$ -m beas  # all job events sent via email
+##$ -q long.q
+##$ -q devel.q
+
+export GEOTOPOTIM2_TEMP_DIR=/tmp/geotopOtim2_tempdir
+export GEOTOPOTIM2_SAVE_DIR=/home/ecordano/Simulations/MonaLisaSims
+
+mkdir -p $GEOTOPOTIM2_TEMP_DIR
+mkdir -p $GEOTOPOTIM2_SAVE_DIR
+
+export I_MPI_PIN_PROCESSOR_LIST=1,14,9,6,5,10,13,2,3,12,11,4,7,8,15,0
+
+
+ 
+mpirun -machinefile $TMPDIR/machines -np $NSLOTS R CMD BATCH pso_example_script_monalisa_vsmle2.R
+
+mkdir $GEOTOPOTIM2_SAVE_DIR/saved
+mv $GEOTOPOTIM2_SAVE_DIR $GEOTOPOTIM2_SAVE_DIR/saved
+
+rm -rf $GEOTOPOTIM2_TEMP_DIR
+
+
