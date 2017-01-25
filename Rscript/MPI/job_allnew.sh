ece920e991e7a4c06861b83f503e201fc51f09bd
Senoner Samuel <samuel.senoner@eurac.edu>
2017-01-25 16:18:40 +0100

    updated jobscript to reflect changes introduced in nompi

--- Rscript/MPI/job_allnew.sh
+++ Rscript/MPI/job_allnew.sh
@@ -1,9 +1,11 @@
-#$ -N geotopoptim2_MonaLisa_array_20170113
+#$ -N geotopoptim2_MonaLisa_array
 #$ -V
 #$ -pe mpich 32
-#$ -M emanuele.cordano@gmail.com,giacomo.bertoldi@eurac.edu
+#$ -M emanuele.cordano@gmail.com,giacomo.bertoldi@eurac.edu,samuel.senoner@eurac.edu
 #$ -m beas  # all job events sent via email
 #$ -l h_rt=10:00:00
+#$ -o $HOME/Simulations/MonaLisaSims/
+#$ -e $HOME/Simulations/MonaLisaSims/
 #$ -t 1-4
 
 #### bash script for jon to optimize several 1D GEOtop simulations with GEOtop Optim for monalisa station
@@ -16,19 +18,19 @@ export GEOTOP_FOLDER=$(awk "NR==$SGE_TASK_ID" $GEOTOP_FOLDERS)
 export GEOTOPOPTIM2_TEMP_DIR=/tmp/geotopOtim2_tempdir
 
 ### folder where you find the final optimized simulation
-export GEOTOPOPTIM2_SAVE_DIR=/home/lv70864/gbertoldi/Simulations/MonaLisaSims
+export GEOTOPOPTIM2_SAVE_DIR=$HOME/Simulations/MonaLisaSims
 
 ### directory where there is the Monalisa project in github with the input data
-export GEOTOPOPTIM2_PROJECT_DIR=/home/lv70864/gbertoldi/Simulations/MonaLisa
+export GEOTOPOPTIM2_PROJECT_DIR=$HOME/Simulations/MonaLisa
 
-mkdir -p $GEOTOPOTIM2_TEMP_DIR
-mkdir -p $GEOTOPOTIM2_SAVE_DIR
+mkdir -p $GEOTOPOPTIM2_TEMP_DIR
+#mkdir -p $GEOTOPOTIM2_SAVE_DIR
 
 export I_MPI_PIN_PROCESSOR_LIST=1,14,9,6,5,10,13,2,3,12,11,4,7,8,15,0
 
 
 #### command to launch R in background whth the R script for launching the simulation pso_monalisa.R
-mpirun -machinefile $TMPDIR/machines -np $NSLOTS R CMD BATCH pso_monalisa.R pso_monalisa_$GEOTOP_FOLDER.Rout
+mpirun -machinefile $TMPDIR/machines -np $NSLOTS R CMD BATCH pso_monalisa.R $GEOTOPOPTIM2_SAVE_DIR/pso_monalisa_$GEOTOP_FOLDER.Rout
 
 
 #####R CMD BATCH pso_example_script_monalisa_vsmle2.R  
