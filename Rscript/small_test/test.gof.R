a782e4494ecf83d25b4c81a9bd28ab2796448a39
yakopuz <giacomo.bertoldi@eurac.edu>
2017-02-15 17:05:47 +0100

    change path

--- Rscript/small_test/test.gof.R
+++ Rscript/small_test/test.gof.R
@@ -12,11 +12,11 @@ library(geotopOptim2)
 
 #wpath <-  '/home/ecor/local-projects/MonaLisa/run/DOMEF_1500_Optim_005' 
 
-wpath <- "C:\\Users\\GBertoldi\\Documents\\Simulations_local\\MonaLisaSims_20170207\\DOMEF_1500_Optim_005"
+wpath <- "/home/gbertoldi/Simulations/NEPAS_2000_001"
 
 ##wpath <- system.file('geotop-simulation/B2site',package="geotopOptim2")
-bin <-   "/home/ecor/local/geotop/GEOtop/bin/geotop-2.0.0"
-runpath <- "/home/ecor/temp/geotopOptim_tests"
+bin <-   "geotop_se27xx"
+runpath <- "/home/gbertoldi/Simulations/tests"
 
 var <- 'soil_moisture_content_50'
 
@@ -24,6 +24,7 @@ param <- NULL ##c(N=1.4,Alpha=0.0021,ThetaRes=0.05)
 ssout <- geotopGOF(x=param,run.geotop=TRUE,bin=bin,
 		simpath=wpath,runpath=runpath,clean=TRUE,data.frame=TRUE,
 		level=1,intern=TRUE,temporary.runpath=TRUE)
-
+		
+ssout
 
 #
