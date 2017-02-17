bebf2f7a680277fa93bb187812a96078f34497b9
ecor <emanuele.cordano@gmail.com>
2017-02-17 11:21:02 +0100

    test.gof

--- Rscript/small_test/test.gof.R
+++ Rscript/small_test/test.gof.R
@@ -9,6 +9,8 @@
 
 rm(list=ls())
 library(geotopOptim2)
+#source('/home/ecor/Dropbox/R-packages/geotopOptim2/R/geotop.execution.R')
+#source('/home/ecor/Dropbox/R-packages/geotopOptim2/R/geotop.gof.2.R') 
 
 wpath <-  '/home/ecor/local-projects/MonaLisa/run/DOMEF_1500_Optim_005' 
 
@@ -18,10 +20,10 @@ runpath <- "/home/ecor/temp/geotopOptim_tests"
 
 var <- 'soil_moisture_content_50'
 
-param <- NULL ##c(N=1.4,Alpha=0.0021,ThetaRes=0.05) 
+param <- c(N=1.4,Alpha=0.0021,ThetaRes=0.05,LOG10_SOIL__NormalHydrConductivity=-1) 
 ssout <- geotopGOF(x=param,run.geotop=TRUE,bin=bin,
-		simpath=wpath,runpath=runpath,clean=TRUE,data.frame=TRUE,
-		level=1,intern=TRUE,temporary.runpath=TRUE)
+		simpath=wpath,runpath=runpath,clean=FALSE,data.frame=TRUE,
+		level=1,intern=TRUE,temporary.runpath=FALSE)
 
 
 #
