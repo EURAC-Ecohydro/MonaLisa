f0505e3c7dc0761a08cfe5bfd58a9828ceb1124a
ecor <emanuele.cordano@gmail.com>
2017-04-15 20:26:05 +0200

    run-Kaltern

---
+++ run/Kaltern/optim/job_Kaltern_PSO.sh
@@ -0,0 +1,39 @@
+#$ -N Kaltern_geotopoptim2_MonaLisa_array
+#$ -V
+#$ -pe mpich 128 # number of cores: 
+#$ -M emanuele.cordano@gmail.com,giacomo.bertoldi@eurac.edu,samuel.senoner@eurac.edu
+#$ -m beas  # all job events sent via email
+#$ -l h_rt=4:00:00
+#$ -o $HOME/Simulations/MonaLisaSims/
+#$ -e $HOME/Simulations/MonaLisaSims/
+
+#### bash script for jon to optimize several 1D GEOtop simulations with GEOtop Optim for monalisa station
+
+# name of the optimization script. Ir should be in the same folder of the present bash script
+export PSO_SCRIPT=pso_Kaltern.R
+
+### root directory where there are the input data: the base simulation and the list of parameters that you optimize 
+export GEOTOPOPTIM2_PROJECT_DIR=$HOME/Simulations/MonaLisa
+
+# the file folders are the base simulation names
+export GEOTOP_FOLDER=Kaltern
+
+### temporary working directory. Be aware that sometimes the folder tmp should be emptied
+export GEOTOPOPTIM2_TEMP_DIR=/tmp/geotopOtim2_tempdir
+
+### folder where you find the final optimized simulation
+export GEOTOPOPTIM2_SAVE_DIR=$HOME/Simulations/MonaLisaSims
+
+
+mkdir -p $GEOTOPOPTIM2_TEMP_DIR
+mkdir -p $GEOTOPOPTIM2_SAVE_DIR/$GEOTOP_FOLDER
+
+export I_MPI_PIN_PROCESSOR_LIST=1,14,9,6,5,10,13,2,3,12,11,4,7,8,15,0
+
+
+#### command to launch R in background whth the R script for launching the simulation pso_monalisa.R
+mpirun -machinefile $TMPDIR/machines -np $NSLOTS R CMD BATCH $PSO_SCRIPT $GEOTOPOPTIM2_SAVE_DIR/$GEOTOP_FOLDER/pso_monalisa_$GEOTOP_FOLDER.Rout
+
+
+
+
