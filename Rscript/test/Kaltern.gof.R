179899b09cdd46bf6448e79ab018c2b89aed7632
ecor <emanuele.cordano@gmail.com>
2017-04-27 20:22:13 +0200

    corrected Kaltern StartDate

--- Rscript/test/Kaltern.gof.R
+++ Rscript/test/Kaltern.gof.R
@@ -9,8 +9,8 @@
 
 rm(list=ls())
 library(geotopOptim2)
-#source('/home/ecor/Dropbox/R-packages/geotopOptim2/R/geotop.execution.R')
-#source('/home/ecor/Dropbox/R-packages/geotopOptim2/R/geotop.gof.2.R') 
+source('/home/ecor/Dropbox/R-packages/geotopOptim2/R/geotop.execution.R')
+source('/home/ecor/Dropbox/R-packages/geotopOptim2/R/geotop.gof.2.R') 
 
 #wpath <-  '/home/ecor/local-projects/MonaLisa/run/DOMEF_1500_Optim_005' 
 
@@ -24,7 +24,7 @@ var <- 'soil_moisture_content_50'
 
 param <- c(N=1.4,Alpha=0.0021,ThetaRes=0.05,LOG10_SOIL__NormalHydrConductivity=-1) 
 ssout <- geotopGOF(x=param,run.geotop=TRUE,bin=bin,simpath=wpath,runpath=runpath,clean=FALSE,data.frame=TRUE,
-level=1,intern=TRUE,temporary.runpath=FALSE,zlayer.formatter="z%04d")
+level=1,intern=TRUE,temporary.runpath=FALSE,zlayer.formatter="z%04d",target=var)
 
 stop("KKK")
 
